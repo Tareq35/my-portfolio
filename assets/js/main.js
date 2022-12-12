816e8060cb40ca7191d7e1cecf9a957730e910f9
Tareq35 <a.t.tariqulislam@gmail.com>
2022-12-12 23:42:08 +0600

    code updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,6 +61,22 @@
   onscroll(document, navbarlinksActive)
 
   /**
+   * Hero type effect
+   */
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+  /**
    * Scrolls to an element with header offset
    */
   const scrollto = (el) => {
